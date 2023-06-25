2b5407c13d3454e2802e5b639fd2a1eace5a52fc
gbsantstech <contatogbsants@outlook.com>
2023-06-25 11:43:28 -0300

    api-beer

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const BeerApp = () => {
+  const [beerData, setBeerData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://random-data-api.com/api/beer/random_beer');
+      setBeerData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderBeerItem = ({ item }) => (
+    <View style={{ marginVertical: 10 }}>
+      <Text>Brand: {item.brand}</Text>
+      <Text>Name: {item.name}</Text>
+      <Text>Style: {item.style}</Text>
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Captura cerveja" onPress={fetchData} />
+      {beerData && (
+        <FlatList
+          data={[beerData]}
+          keyExtractor={() => beerData.id.toString()}
+          renderItem={renderBeerItem}
+        />
+      )}
+    </View>
+  );
+};
+
+export default BeerApp;
+
